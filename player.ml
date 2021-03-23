d8ca1c1dbd66be9e8f8132175388923a63fec942
awc93 <awc93@cornell.edu>
2021-03-23 14:12:27 -0400

    started player.ml

---
+++ player.ml
@@ -0,0 +1,29 @@
+open Tile
+
+type player = {
+  name : string;
+  number : int;
+  played_valid_meld : bool;
+  rack : rack;
+  past_racks : rack list;
+  score : int;
+}
+
+and rack = tile list
+
+(* Input is a player association list in the format [(player_num *
+   player_name);..]. Example: [(1, "Clarkson"); (2, "Lee")]. *)
+let rec make_players acc stack = function
+  | [] -> acc
+  | (number, name) :: t ->
+      make_players
+        ({
+           name;
+           number;
+           played_valid_meld = false;
+           rack = Tile.make_tile_rack stack;
+           past_racks = [];
+           score = 0;
+         }
+        :: acc)
+        stack t
---
+++ player.mli
@@ -0,0 +1,35 @@
+(* type player = { name : string ; number : int ; played_initial_meld :
+   bool ; rack : rack ; start_turn_rack : rack; past_rack : rack list ;
+   score : int }
+
+   function make_player : initializes a new player
+
+   type rack = tile list
+
+   function fill_rack : adds a list of 14 tiles by Pile.deal_rack and
+   assigns it to player.rack
+
+   function sort_by_color : sorts the rack based on color first then
+   ascending numbers, jokers last.
+
+   function sort_by_number : sorts the rack based off number first then
+   color, jokers last
+
+   function print_rack : pretty print rack
+
+   function rack_size : returns number of tiles in rack
+
+   function empty_past_rack : creates a new player object with the
+   “past_rack” field set to [ ]
+
+   function update_past_rack : creates a new player object with a new
+   rack list with current “rack” field value appended to the “past_rack”
+   field.
+
+   function undo_past_rack : creates a new player object with the last
+   rack element in the “past_rack” field deleted and set to the “rack”
+   field.
+
+   function reset_current_turn_rack : creates a new player object with
+   the first rack element in the “past_rack” field and calls
+   empty_past_rack *)
