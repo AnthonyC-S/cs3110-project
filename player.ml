a45514d2e78c3c197bb93d14d16d3dd055f167c3
Christy Song <cs966@cornell.edu>
2021-03-27 15:23:49 -0400

    specs for player.ml

--- player.ml
+++ player.ml
@@ -28,32 +28,41 @@ let rec make_players acc stack = function
         :: acc)
         stack t
 
-let empty_past_rack player = { player with past_racks = [] }
+(** [empty_past_rack p] is player [p] with the past_racks field set to
+    an empty list of racks. *)
+let empty_past_rack p = { p with past_racks = [] }
 
-let update_past_rack player =
-  { player with past_racks = player.past_racks @ [ player.rack ] }
+(** [update_past_rack p] is player [p] with the current rack [r] of [p]
+    appended at the end of past_racks list. *)
+let update_past_rack p =
+  { p with past_racks = p.past_racks @ [ p.rack ] }
 
 exception EmptyRack
 
-(* helper *)
+(** [last_ele_lst_rest lst] is a pair with the first element being the
+    last element of [lst] and the second element being the rest of
+    [lst]. *)
 let last_ele_lst_rest lst =
   match List.rev lst with
   | [] -> raise EmptyRack
   | h :: t -> (h, List.rev t)
 
-(* could use nth, but suggested to not use nth? *)
+(* could use nth, but suggested to not use nth?? *)
 
-let undo_past_rack player =
-  let last_ele_rest = last_ele_lst_rest player.past_racks in
-  {
-    player with
-    rack = fst last_ele_rest;
-    past_racks = snd last_ele_rest;
-  }
+(** [undo_past_rack p] is player [p] with rack in [p] set to the last
+    element of its past_racks which is the most recent configuration of
+    rack. The last element is then removed from the past_racks of [p]. *)
+let undo_past_rack p =
+  let last_ele_rest = last_ele_lst_rest p.past_racks in
+  { p with rack = fst last_ele_rest; past_racks = snd last_ele_rest }
 
-(* helper *)
+(** [fst_ele lst] is the first element of [lst]. It raises the
+    EmptryRack exception if [lst] is empty. SHOULD I JUST USE LIST.HD?? *)
 let fst_ele = function [] -> raise EmptyRack | h :: t -> h
 
-let reset_current_turn_rack player =
-  let fst_rack = fst_ele player.past_racks in
-  { (empty_past_rack player) with rack = fst_rack }
+(** [reset_current_turn_rack p] is player [p] with rack configuration
+    set to the initial configuration from the start of its turn and the
+    past_rack emptied out. *)
+let reset_current_turn_rack p =
+  let fst_rack = fst_ele p.past_racks in
+  { (empty_past_rack p) with rack = fst_rack }
