21d99187bd21e65de0d2c65fd268cbe2b747a838
Mina <mh859@cornell.edu>
2021-03-27 22:53:59 -0400

    board and board test

--- board.ml
+++ board.ml
@@ -26,6 +26,20 @@ let rec init_board_aux (acc : board_row list) (rows : string list) =
 
 let init_board () = List.rev (init_board_aux [] rows)
 
+(*Helper for [compare_tiles]
+let get_number x = 
+  match x with 
+  |Tile {number : int; color : color;} -> number
+  |Joker {number : int; color : color;} -> number
+
+
+let compare_tiles x y = 
+  if (get_number x) > (get_number y) then 1 else -1
+  *)  
+  
+  
+(* compare function should sort by num first then color*)
+
 let rec add_tile tile row_letter acc = function
   | [] -> failwith "Row not on board."
   | { row = r; visible = v; tiles = ts } :: t ->
@@ -78,9 +92,25 @@ let valid_run lst =
   && colors_of_t [] lst |> List.sort_uniq compare |> List.length = 1
   && valid_run_aux true (List.sort compare (numbers_of_t [] lst))
 
+  (*acc is true
 let rec valid_board acc = function
   | [] -> acc
   | h :: t ->
       valid_board
         ((List.length h = 0 || valid_run h || valid_group h) && acc)
         t
+*)
+let rec tiles_of_board board = 
+  match board with
+  | [] -> []
+  |h::t -> h.tiles :: (tiles_of_board t)
+
+let rec valid_rows acc tile_rows = 
+  match tile_rows with 
+  | [] -> acc
+  | h :: t ->
+      valid_rows((List.length h = 0 || valid_run h || valid_group h) && acc) t
+
+let valid_board acc board = 
+  let tile_rows = tiles_of_board board in 
+  valid_rows acc tile_rows
--- board.mli
+++ board.mli
@@ -1,13 +1,22 @@
+(** The abstract type representing board. *)
 type b
 
+(** The type of a row on the board. *)
 type board_row
 
+(** [init_board] is the initial state of the board with no tiles. *)
 val init_board : unit -> b
 
-val valid_group : Tile.t list -> bool
+(* val valid_board : bool -> Tile.t list list -> bool *)
 
-val valid_run : Tile.t list -> bool
+(** [valid_board acc board] is the result of removing a tile from the board *)
+val valid_board : bool -> b -> bool
 
-val valid_board : bool -> Tile.t list list -> bool
+(** [add_tile tile row_letter acc] is the result of adding a tile to the board *)
+val add_tile : Tile.t  -> string -> board_row list -> b -> b
+
+
+(** [remove_tile tile row_letter acc] is the result of removing a tile from the board *)
+val remove_tile : Tile.t  -> string -> board_row list -> b -> b
 
 (* val add_tile_to_board : Tile.t -> string -> board_row list -> b -> b *)
