e28276d82e3257dc1dee5859f59762852930b098
Anthony Coffin-Schmitt <awc93@cornell.edu>
2021-04-26 20:14:28 -0400

    Added color sort

--- tile.ml
+++ tile.ml
@@ -227,14 +227,17 @@ let get_tile_rec = function Tile t -> t | Joker t -> t
 let color_compare c1 c2 =
   compare (get_tile_rec c1).color (get_tile_rec c2).color
 
-let rec color_sort k r b o j = function
-  | [] ->
-      List.flatten
-        (List.map (fun x -> List.sort compare x) [ k; r; b; o; j ])
-  | h :: t -> (
-      match (get_tile_rec h).color with
-      | Black -> color_sort (h :: k) r b o j t
-      | Red -> color_sort k (h :: r) b o j t
-      | Blue -> color_sort k r (h :: b) o j t
-      | Orange -> color_sort k r b (h :: o) j t
-      | None -> color_sort k r b o (h :: j) t)
+let color_sort t_lst =
+  let rec sort k r b o j = function
+    | [] ->
+        List.flatten
+          (List.map (fun x -> List.sort compare x) [ k; r; b; o; j ])
+    | h :: t -> (
+        match (get_tile_rec h).color with
+        | Black -> sort (h :: k) r b o j t
+        | Red -> sort k (h :: r) b o j t
+        | Blue -> sort k r (h :: b) o j t
+        | Orange -> sort k r b (h :: o) j t
+        | None -> sort k r b o (h :: j) t)
+  in
+  sort [] [] [] [] [] t_lst
