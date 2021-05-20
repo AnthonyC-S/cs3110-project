7ff15486809cf38a1867c37c2fa3e27d593ff965
Anthony Coffin-Schmitt <awc93@cornell.edu>
2021-05-20 19:04:11 -0400

    added page to tutorial

--- tutorial.ml
+++ tutorial.ml
@@ -6,13 +6,13 @@ open Textgui
 let board_with_all_tiles =
   [
     (Blue, "A");
-    (Blue, "B");
-    (Orange, "C");
-    (Orange, "D");
-    (Red, "E");
-    (Red, "F");
-    (Black, "G");
-    (Black, "H");
+    (Blue, "C");
+    (Orange, "G");
+    (Orange, "I");
+    (Red, "M");
+    (Red, "O");
+    (Black, "S");
+    (Black, "U");
   ]
 
 let rec mult_add_to_board board row = function
@@ -30,8 +30,8 @@ let full_board () =
              |> List.rev))
           t
   in
-  let main_tiles = aux [] board_with_all_tiles in
-  mult_add_to_board main_tiles "I"
+  let main_tiles = aux (init_board ()) board_with_all_tiles in
+  mult_add_to_board main_tiles "Y"
     [ make_t "J" 100 None; make_t "J" 100 None ]
 
 let start_state =
@@ -47,11 +47,18 @@ let start_tutorial =
     (g "  Welcome to the Camlkub Tutorial!"
     ^ "\n\
       \  This tutorial will teach you how to play the game and the \
-       commands used.\n\n\
+       commands used.\n\
+      \  Camlkub is the same as Rummikub, we named it Camlkub since it \
+       was programmed in OCaml. \n\n\
       \  To quit this tutorial at any time, enter \"q\". To continue \
        through the tutorial, press enter.\n")
 
 let tutorial_1 =
-  build_board { start_state with board = full_board () } "Full Board!"
+  build_board
+    { start_state with board = full_board () }
+    "  Camlkub is played with 106 different tiles.\n\
+    \  There are two sets of tiles numbered 1 to 13 in the colors \
+     Blue, Orange, Red, and Black.\n\
+    \  There are also two joker tiles that can be any color or number.\n"
 
 let tutorial_pages = [ start_tutorial; tutorial_1 ]
